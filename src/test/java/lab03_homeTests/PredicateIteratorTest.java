dc16fd8c7f14ff3096c38ac34705ef1f44a375fa
Michal Partyka <partyks@gmail.com>
2013-07-30 18:27:52 +0200

    Escalate two bugs :-). Starting from instantiating :P, you haven't have tested it!

---
+++ src/test/java/lab03_homeTests/PredicateIteratorTest.java
@@ -0,0 +1,43 @@
+package lab03_homeTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.agh.jtp.lab03_home.Predicate;
+import pl.agh.jtp.lab03_home.PredicateIterator;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * @author Michal Partyka
+ */
+public class PredicateIteratorTest {
+
+    private PredicateIterator<Integer> instance;
+
+    @Before
+    public void setUp( ) {
+
+    }
+
+    @Test
+    public void hasNextShouldReturnFalseIfNoElementsApplyToPredicate( ) {
+        //given
+        List<Integer> list = Arrays.asList(1, 4, 5, 2, 1, 4, 12, 1, 41, 12);
+        instance = new PredicateIterator<Integer>(list.iterator(), new Predicate<Integer>() {
+            @Override
+            public boolean apply(Integer element) {
+                return element > 100;
+            }
+        });
+
+        //when
+        boolean hasNext = instance.hasNext();
+
+        //then
+        assertEquals(false, hasNext);
+    }
+}
